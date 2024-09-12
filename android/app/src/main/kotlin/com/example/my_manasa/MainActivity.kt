7aaa1e581b98cd1efc0311bcea262ec487eaee98
AdhamEhabAi <adhame962@gmail.com>
2024-09-12 23:36:54 +0300

    finished

--- android/app/src/main/kotlin/com/example/my_manasa/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_manasa/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.example.my_manasa
 
+import android.os.Bundle
+import android.view.WindowManager.LayoutParams
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Prevent screenshots and video recording
+        window.setFlags(
+            LayoutParams.FLAG_SECURE,
+            LayoutParams.FLAG_SECURE
+        )
+    }
 }
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,13 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        // Prevent screenshots and video recording
+        self.window?.layer.contentsRect = CGRect(x: -1, y: -1, width: 0, height: 0)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
