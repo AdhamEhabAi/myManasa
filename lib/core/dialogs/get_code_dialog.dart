ed80a30c01d28bec1031f51e78443b40fe754a9d
AdhamEhabAi <adhame962@gmail.com>
2024-09-13 03:08:16 +0300

    finished

--- lib/core/dialogs/get_code_dialog.dart
+++ lib/core/dialogs/get_code_dialog.dart
@@ -10,6 +10,7 @@ import 'package:my_manasa/core/widgets/custom_button.dart';
 import 'package:my_manasa/core/widgets/info_textfield.dart';
 import 'package:my_manasa/core/widgets/toast_m.dart';
 import 'package:my_manasa/features/authentication/presentation/manager/auth_cubit.dart';
+import 'package:my_manasa/features/authentication/presentation/views/login_view.dart';
 
 class GetCodeDialog extends StatelessWidget {
   const GetCodeDialog({
@@ -23,6 +24,7 @@ class GetCodeDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String userID = context.read<AuthCubit>().userModel!.id;
     return Dialog(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(30.r),
@@ -30,73 +32,99 @@ class GetCodeDialog extends StatelessWidget {
       child: Container(
         width: MediaQuery.of(context).size.width * .8,
         padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'أدخل الكود',
-              style: Styles.semiBold20.copyWith(
-                  color: Colors.black, decoration: TextDecoration.none),
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.w),
-              child: InfoTextField(
-                width: double.infinity,
-                textEditingController: textEditingController,
-                hintText: '',
-              ),
-            ),
-            SizedBox(height: 20.h),
-            BlocConsumer<CodeCubit, CodeState>(
-              listener: (context, state) {
-                if (state is CodeSuccess) {
-                  Get.back(); // Close the current dialog
-                  showDialog(
-                    context: context,
-                    builder: (context) => const BuyCompletedDialog(),
-                  );
-                } else if (state is CodeError) {
-                  Get.back(); // Close the current dialog
-                  showDialog(
-                    context: context,
-                    builder: (context) => const WrongCodeDialog(),
-                  );
-                }
-              },
-              builder: (context, state) {
-                if (state is CodeLoading) {
-                  return const CircularProgressIndicator(); // Show loading indicator
-                }
-                return Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40.0.w),
-                  child: CustomButton(
+        child: userID == '66851'
+            ? Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'سجل الدخول لشراء الكورس',
+                    style: Styles.semiBold16.copyWith(
+                        color: Colors.black, decoration: TextDecoration.none),
+                  ),
+                  SizedBox(height: 20.h),
+                  CustomButton(
                     text: Text(
-                      'شراء',
-                      style: Styles.semiBold14.copyWith(color: Colors.white),
+                      'سجل الدخول',
+                      style:
+                      Styles.semiBold14.copyWith(color: Colors.white),
                     ),
                     borderRadius: 10.r,
                     onPressed: () {
-                      final code = textEditingController.text.trim();
-                      if (code.isNotEmpty) {
-                        String userId = context.read<AuthCubit>().userModel!.id;
-
-                        // Call purchaseCourse with or without idCourse based on its availability
-                        context.read<CodeCubit>().purchaseCourse(
-                          userId: userId,
-                          code: code,
-                          idCourse: idCourse, // Pass idCourse to the function
+                      Get.to(LoginView());
+                    },
+                  )
+                ],
+              )
+            : Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'أدخل الكود',
+                    style: Styles.semiBold20.copyWith(
+                        color: Colors.black, decoration: TextDecoration.none),
+                  ),
+                   SizedBox(height: 20.h),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.w),
+                    child: InfoTextField(
+                      width: double.infinity,
+                      textEditingController: textEditingController,
+                      hintText: '',
+                    ),
+                  ),
+                  SizedBox(height: 20.h),
+                  BlocConsumer<CodeCubit, CodeState>(
+                    listener: (context, state) {
+                      if (state is CodeSuccess) {
+                        Get.back(); // Close the current dialog
+                        showDialog(
+                          context: context,
+                          builder: (context) => const BuyCompletedDialog(),
                         );
-                      } else {
-                        ToastM.show('يرجى إدخال كود صالح.');
+                      } else if (state is CodeError) {
+                        Get.back(); // Close the current dialog
+                        showDialog(
+                          context: context,
+                          builder: (context) => const WrongCodeDialog(),
+                        );
                       }
                     },
+                    builder: (context, state) {
+                      if (state is CodeLoading) {
+                        return const CircularProgressIndicator(); // Show loading indicator
+                      }
+                      return Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 40.0.w),
+                        child: CustomButton(
+                          text: Text(
+                            'شراء',
+                            style:
+                                Styles.semiBold14.copyWith(color: Colors.white),
+                          ),
+                          borderRadius: 10.r,
+                          onPressed: () {
+                            final code = textEditingController.text.trim();
+                            if (code.isNotEmpty) {
+                              String userId =
+                                  context.read<AuthCubit>().userModel!.id;
+
+                              // Call purchaseCourse with or without idCourse based on its availability
+                              context.read<CodeCubit>().purchaseCourse(
+                                    userId: userId,
+                                    code: code,
+                                    idCourse:
+                                        idCourse, // Pass idCourse to the function
+                                  );
+                            } else {
+                              ToastM.show('يرجى إدخال كود صالح.');
+                            }
+                          },
+                        ),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
-          ],
-        ),
+                ],
+              ),
       ),
     );
   }
